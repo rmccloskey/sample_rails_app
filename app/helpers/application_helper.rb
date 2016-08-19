1a38307adfc13009aefcf1846346480da4dccebb
Ryan McCloskey <rmccloskey8@gmail.com>
2016-08-19 12:44:53 +0000

    Added a full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
-module ApplicationHelper
+module ApplicationHelper                                  # Modules used to package methods, then used in Ruby classes with 'include'
+  # Returns the full title on a page-by-page basis        # Documentation Comment
+  def full_title(page_title = '')                         # Method definition, optional argument
+    base_title = "Ruby on Rails Tutorial Sample App"      # Variable assignment
+    if page_title.empty?                                  # Boolean test
+      base_title                                          # Implicit return
+    else
+      page_title + " | " + base_title                     # String concatenation
+    end
+  end
 end
