65bc78cab24f6e5970560e0da8b7d18a2c44c876
Ryan McCloskey <rmccloskey8@gmail.com>
2016-08-19 16:46:52 +0000

    Fixed small error

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,11 +1,7 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
-  
+
   test "should get home" do
     get root_path
     assert_response :success
@@ -24,10 +20,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
-  
   test "should get contact" do
     get contact_path
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 end
+
+
