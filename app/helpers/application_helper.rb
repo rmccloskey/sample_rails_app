7443317d4821ef50682b3de2e25ab3275b1f0950
Ryan McCloskey <rmccloskey8@gmail.com>
2016-08-19 12:55:26 +0000

    Fixed error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
-module ApplicationHelper                                  # Modules used to package methods, then used in Ruby classes with 'include'
-  # Returns the full title on a page-by-page basis        # Documentation Comment
-  def full_title(page_title = '')                         # Method definition, optional argument
-    base_title = "Ruby on Rails Tutorial Sample App"      # Variable assignment
-    if page_title.empty?                                  # Boolean test
-      base_title                                          # Implicit return
+module ApplicationHelper
+
+  # Returns the full title on a per-page basis.       # Documentation comment
+  def full_title(page_title = '')                     # Method def, optional arg
+    base_title = "Ruby on Rails Tutorial Sample App"  # Variable assignment
+    if page_title.empty?                              # Boolean test
+      base_title                                      # Implicit return
     else
-      page_title + " | " + base_title                     # String concatenation
+      page_title + " | " + base_title                 # String concatenation
     end
   end
 end
